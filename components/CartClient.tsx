14a31a480b06e439f6e8aee76cb342a599a87e1d
Daryl <borismbakop611@gmail.com>
2024-06-29 18:54:50 +0100

    Update CartClient.tsx

--- components/CartClient.tsx
+++ components/CartClient.tsx
@@ -58,7 +58,7 @@ const CartClient = ({  User
       
             className="mb-2 w-full flex flex-col justify-center items-center bg-blue-700 hover:bg-blue-900 text-white px-4 py-2 rounded-lg"
           >
-            Commander
+            Connecter vous afin de passer la commande
           </Button>
         ) : (
           <Button
@@ -66,7 +66,7 @@ const CartClient = ({  User
            
             className="mb-2 w-full flex flex-col justify-center items-center bg-blue-700 hover:bg-blue-900 text-white px-4 py-2 rounded-lg"
           >
-            Connecter vous afin de passer la commande
+            Commander
           </Button>
         )}
 
